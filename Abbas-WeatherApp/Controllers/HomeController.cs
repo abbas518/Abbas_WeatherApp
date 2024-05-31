8a3db26c550ae1ccd41c457725913dbf8389ec92
Abbas Muhammad <abbas51813@gmail.com>
2024-05-31 15:46:57 +0300

    Initial commit

--- Abbas-WeatherApp/Controllers/HomeController.cs
+++ Abbas-WeatherApp/Controllers/HomeController.cs
@@ -1,32 +1,136 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using Abbas_WeatherApp.Models;
+using System.Net.Http;
 
-namespace Abbas_WeatherApp.Controllers;
-
-public class HomeController : Controller
+namespace Abbas_WeatherApp.Controllers
 {
-    private readonly ILogger<HomeController> _logger;
 
-    public HomeController(ILogger<HomeController> logger)
-    {
-        _logger = logger;
-    }
 
-    public IActionResult Index()
-    {
-        return View();
-    }
 
-    public IActionResult Privacy()
+    public class HomeController : Controller
     {
-        return View();
-    }
+        public string apiKey = "YOUR_API_KEY";
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        private readonly HttpClient _httpClient = new HttpClient();
+
+        public HomeController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClient = httpClientFactory.CreateClient();
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult?> GetWeatherByLocation(string location)
+        {
+            if (string.IsNullOrWhiteSpace(location))
+            {
+                return null;
+            }
+
+            string apiUrl = $"weather?q={location}&appid={apiKey}&units=metric";
+            return await GetWeather(apiUrl);
+        }
+
+        public async Task<IActionResult?> GetWeatherByLatLong(string lat, string lon)
+        {
+            if (string.IsNullOrWhiteSpace(lat) || string.IsNullOrWhiteSpace(lon))
+            {
+                return View();
+            }
+
+            string apiUrl = $"weather?lat={lat}&lon={lon}&appid={apiKey}&units=metric";
+            return await GetWeather(apiUrl);
+        }
+
+        private async Task<IActionResult?> GetWeather(string apiUrl)
+        {
+            try
+            {
+
+                _httpClient.BaseAddress = new Uri("https://api.openweathermap.org/data/2.5/");
+                HttpResponseMessage response = await _httpClient.GetAsync(apiUrl);
+
+                WeatherData weatherData = new WeatherData();
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string responseBody = await response.Content.ReadAsStringAsync();
+                    weatherData = JsonConvert.DeserializeObject<WeatherData>(responseBody);
+                }
+                else
+                {
+                    return Json(null);
+                }
+
+                return Json(weatherData);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception caught: {ex}");
+                return StatusCode(500, "Internal Server Error");
+            }
+        }
+
+        public async Task<IActionResult?> Get5DayForecastByLocation(string location)
+        {
+            if (string.IsNullOrWhiteSpace(location))
+            {
+                return null;
+            }
+
+            string apiUrl = $"forecast?q={location}&appid={apiKey}&units=metric";
+            return await Get5DayForecastWeather(apiUrl);
+        }
+
+        public async Task<IActionResult?> Get5DayForecastByLatLong(string lat, string lon)
+        {
+            if (string.IsNullOrWhiteSpace(lat) || string.IsNullOrWhiteSpace(lon))
+            {
+                return View();
+            }
+
+            string apiUrl = $"forecast?lat={lat}&lon={lon}&appid={apiKey}&units=metric";
+            return await Get5DayForecastWeather(apiUrl);
+        }
+
+        private async Task<IActionResult?> Get5DayForecastWeather(string apiUrl)
+        {
+            try
+            {
+                _httpClient.BaseAddress = new Uri("https://api.openweathermap.org/data/2.5/");
+                HttpResponseMessage response = await _httpClient.GetAsync(apiUrl);
+
+                List<NextFiveDayForecastData.List> nextFiveDaysList = new List<NextFiveDayForecastData.List>();
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string responseBody = await response.Content.ReadAsStringAsync();
+                    NextFiveDayForecastData.Root forecastData = JsonConvert.DeserializeObject<NextFiveDayForecastData.Root>(responseBody);
+
+                    // Extract the list of the next 5 days
+                    nextFiveDaysList = forecastData.list.Take(80).ToList();
+
+                    return Json(nextFiveDaysList);
+                }
+                else
+                {
+                    return Json(null);
+                }
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "Internal Server Error");
+            }
+        }
+
+
+
+
+
     }
 }
-
